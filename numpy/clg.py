e41277928f3f682f7c1d25d928a006f87726137c
Md sahil <mdsahil880999@gmail.com>
2026-05-29 21:29:21 +0530

    Refactor comments in clg.py for clarity
    Updated comments to clarify array dimensions and usage of ndim and shape.

--- numpy/clg.py
+++ numpy/clg.py
@@ -1,6 +1,6 @@
 import numpy as np
-# x = np.array([[1.5,2.2,5.9,3.5,7.7],[1.5,2.2,5.9,3.5,7.7],[1.5,2.2,5.9,3.5,7.7],[1.5,2.2,5.9,3.5,7.7]])
-x = np.array([1.5,2.2,5.9,3.5,7.7])
-print(x.ndim)
+# x = np.array([[1.5,2.2,5.9,3.5,7.7],[1.5,2.2,5.9,3.5,7.7],[1.5,2.2,5.9,3.5,7.7],[1.5,2.2,5.9,3.5,7.7]])  # --> ndim = 2 bcoz there is 2 middle bracket
+x = np.array([1.5,2.2,5.9,3.5,7.7])  # ndim = 1
+print(x.ndim)  #ndim -> used for showing how many dimensional array you have created like 1D, 2D, 3D...... nD
 print(x.shape)
 print(x.size)
